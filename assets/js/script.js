bb5198c513a75ddae822f510c7d361c482e535ae
Darshana Balai <darshana.balai@gmail.com>
2023-05-28 16:23:36 +0000

    Add query selector

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const addInput = document.querySelector(".add-task");
+const addButton = document.querySelector(".add-button");
+const options = document.querySelector(".options");
+const toDoList = document.querySelector(".list");
