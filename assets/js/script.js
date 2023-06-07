43ba143b428030b823133af96c4369fa7a9e71a3
Darshana Balai <darshana.balai@gmail.com>
2023-06-07 22:32:58 +0000

    Add new listinput div in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@ const addInput = document.querySelector(".add-task");
 const addButton = document.querySelector(".add-button");
 const options = document.querySelector(".options");
 const toDoList = document.querySelector(".list");
+
+addButton.addEventListener('submit', listEntries)
+
+function listEntries (event){
+event.preventDefault();
+
+let listEntryDiv = document.createElement("div");
+listEntryDiv.classList.add("listEntryDiv");
+let listEntry =document.createElement("li");
+listEntry.innerHTML= addInput.value;
+listEntry.classList.add("listEntry")
+listEntryDiv.appendChild(listEntry);
+
+}
