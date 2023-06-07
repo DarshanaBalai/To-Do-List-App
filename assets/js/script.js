bfcc24ec572ab12a8b5be8e3c0118c8d1deaa792
Darshana Balai <darshana.balai@gmail.com>
2023-06-07 23:38:28 +0000

    Add delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,26 @@ const addButton = document.querySelector(".add-button");
 const options = document.querySelector(".options");
 const toDoList = document.querySelector(".list");
 
-addButton.addEventListener("click",listEntries)
+addButton.addEventListener("click",listEntries);
 
 function listEntries (event){
 event.preventDefault();
 console.log("done");
-let listEntryDiv = document.createElement("div");
+const listEntryDiv = document.createElement("div");
 listEntryDiv.classList.add("list-entryDiv");
-let listEntry =document.createElement("li");
+const listEntry = document.createElement("li");
 listEntry.innerText= addInput.value;
 listEntry.classList.add("list-entry")
 listEntryDiv.appendChild(listEntry);
 console.log("done");
  
-let doneButton = document.createElement("button");
+const doneButton = document.createElement("button");
 doneButton.innerHTML = <i class="fa-solid fa-square-check"></i>
 doneButton.className.add("doneBtn")
+
+ const deleteButton = document.createElement("button");
+ deleteButton.innerHTML = <i class="fa-solid fa-trash"></i>
+ deleteButton.classList.add("deleteBtn");
+ 
 }
 
