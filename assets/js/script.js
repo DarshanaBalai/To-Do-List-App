fa842b1723dff5658aa1f87e89a8bfe6ceb33622
Darshana Balai <darshana.balai@gmail.com>
2023-06-09 11:13:01 +0000

    Add delete button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+console.log("hello");
+
 const addInput = document.querySelector(".add-task");
 const addButton = document.querySelector(".add-button");
 const options = document.querySelector(".options");
@@ -17,22 +19,51 @@ listEntryDiv.appendChild(listEntry);
 console.log("done");
  
 const editButton = document.createElement("button");
-editButton.innerHTML= <i class="fa-thin fa-pen-to-square"></i>;
+editButton.innerHTML= '<i class="fa-solid fa-pen-to-square"></i>';
 editButton.classList.add("editBtn");
 listEntryDiv.appendChild(editButton)
 
 const doneButton = document.createElement("button");
-doneButton.innerHTML = <i class="fa-solid fa-square-check"></i>
-doneButton.className.add("doneBtn")
+ doneButton.innerHTML =  '<i class="fas fa-check-circle"></i>';
+doneButton.classList.add("doneBtn");
 listEntryDiv.appendChild(doneButton)
 
  const deleteButton = document.createElement("button");
- deleteButton.innerHTML = <i class="fa-solid fa-trash"></i>
+deleteButton.innerHTML = '<i class="fas fa-trash"></i>'
  deleteButton.classList.add("deleteBtn");
  listEntryDiv.appendChild(deleteButton)
 
- list.appendChild(listEntryDiv);
+ toDoList.appendChild(listEntryDiv);
  addInput.value = "";
 
-}
 
+editButton.addEventListener("click", handleEdit);
+
+function handleEdit (event){
+    if(editButton.innerHTML == '<i class="fa-solid fa-pen-to-square"></i>'){
+        editButton.removeAttribute("readonly");
+
+        editButton.innerHTML = '<i class="fa-solid fa-bookmark"></i>';
+      
+        editButton.focus();
+    }else{
+        editButton.innerHTML ='<i class="fa-solid fa-bookmark"></i>';
+        editButton.setAttribute("readonly", "readonly");
+			
+    }
+}
+    deleteButton.addEventListener('click' , handleDelete);
+
+    function handleDelete (event){
+        toDoList.removeChild(listEntryDiv);
+      
+    }
+
+    doneButton.addEventListener('click', handleDone)
+
+    function handleDone (event) {
+        console.log('complete')
+        listEntryDiv.style.textDecoration = "line-through";
+       
+    }
+}
